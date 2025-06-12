74267720c6177d94f588fd49afc84840793e6954
Jules <inezajul@gmail.com>
2025-06-12 16:48:13 +0200

    get the directory to change

--- copilot_shell_script.sh
+++ copilot_shell_script.sh
@@ -8,4 +8,13 @@ if [ -z "$new_name" ]; then
 	exit 1
 fi
 
+# find the directory where the config file is it does this by finding the directory created by create_environment.sh
+reminder_dir=$(find . -maxdepth 1 -type d -name "submission_reminder_*" | head -n 1 )
+
+# If not found, show error
+if [ -z "$reminder_dir" ]; then
+    echo "Could not find the submission_reminder directory."
+    echo "Make sure you ran the create_environment.sh before running this one."
+    exit 1
+fi
 
